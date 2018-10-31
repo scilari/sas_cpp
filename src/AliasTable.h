275d7597643ee7add01251740fe0fe1cef7a9709
scilari <ilari.vallivaara@gmail.com>
2018-10-31 18:50:04 +0200

    Add documentation about the original publication and public interface. Make constructor explicit.

--- src/AliasTable.h
+++ src/AliasTable.h
@@ -5,21 +5,28 @@
 #ifndef SAS_CPP_ALIASTABLE_H
 #define SAS_CPP_ALIASTABLE_H
 
-
 #include <vector>
-
+/**
+  * Kronmal and Peterson 1979 implementation of alias method by Walker (1974, 1977) as described in
+  * "An Analysis of the Alias Method for Discrete Random-Variate Generation" by Smith and Jacobson (2005)
+  *
+  * After initialization with the point mass function (pmf), the object acts as a wrapper for the aliased
+  * value indices and their corresponding probabilities (ratio of the bin).
+  */
 namespace scilari{
 
     class AliasTable {
     public:
-        AliasTable(const std::vector<double>& pmf);
+        /// Constructs alias table by using the given point mass function (pmf). Pmf does not need to be normalized.
+        explicit AliasTable(const std::vector<double>& pmf);
 
+        /// Indices of elements corresponding to the probability mass in aliasProbabilities
         std::vector<int> aliasIndices;
+
+        /// Probabilities corresponding to the aliased values (represented by the indices)
         std::vector<double> aliasProbabilities;
     };
+
 } // namespace scilari
-
-
-
 
 #endif //SAS_CPP_ALIASTABLE_H
