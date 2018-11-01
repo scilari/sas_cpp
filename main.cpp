a4d3f2fda38f1c936b12755d52fb165b39a75213
scilari <ilari.vallivaara@gmail.com>
2018-11-01 17:12:57 +0200

    Update example.

--- main.cpp
+++ main.cpp
@@ -4,30 +4,38 @@
 
 
 int main() {
-    std::vector<int> values = {1, 2, 3, 4};
-    std::vector<double> pmf = {0.1, 0.3, 0.4, 0.2};
+    std::cout << "Hello Systematic Alias Sampler!" << std::endl;
 
+    // Setting up possible values and the corresponding probabilities (point mass function)
+    std::vector<int> values = {0, 1, 2, 3, 4};
+    std::vector<double> pmf = {0.1, 0.3, 0.4, 0.1, 0.1};
+
+    // Constructing the sampler
     scilari::SystematicAliasSampler<int> sampler(values, pmf);
 
-    auto samples = sampler.Sample(100);
-    int f1 = 0, f2 = 0, f3 = 0, f4 = 0;
-    for(int i : samples){
-        if(i == 1) f1++;
-        if(i == 2) f2++;
-        if(i == 3) f3++;
-        if(i == 4) f4++;
+    // Sampling some values and printing the frequencies
+    std::vector<int> sampleCounts = {10, 33, 100, 333, 1000, 3333, 10000};
+
+    for(int sampleCount : sampleCounts){
+        std::cout << "Sampling " << sampleCount << " values..." << std::endl;
+
+        auto samples = sampler.Sample(sampleCount);
+
+        // counting the occurrences
+        std::vector<int> occurrences = {0, 0, 0, 0, 0};
+        for(int s: samples){
+            occurrences[s]++;
+        }
+
+        // printing the empirical ratios
+        for(int i = 0; i < occurrences.size(); ++i){
+            double ratio = occurrences[i]/ static_cast<double>(sampleCount);
+            double expected = pmf[i];
+            std::cout << "Expected ratio of " << i << ": " << expected << " empirical ratio: " << ratio << std::endl;
+        }
+
+        std::cout << std::endl;
     }
-
-    double size = (double) samples.size();
-    double q1 = f1/size;
-    double q2 = f2/size;
-    double q3 = f3/size;
-    double q4 = f4/size;
-
-    std::cout << std::to_string(q1) << ": " << std::to_string(pmf[0])  << std::endl;
-    std::cout << std::to_string(q2) << ": " << std::to_string(pmf[1])  << std::endl;
-    std::cout << std::to_string(q3) << ": " << std::to_string(pmf[2])  << std::endl;
-    std::cout << std::to_string(q4) << ": " << std::to_string(pmf[3])  << std::endl;
 
     return 0;
 }
